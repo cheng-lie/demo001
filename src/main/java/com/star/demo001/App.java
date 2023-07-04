245b6af26cc0720055f3690a91ce184bed95442c
jincl <jincl@sh-db.com.cn>
2023-07-04 14:55:04 +0800

    initialize maven and spring boot project

---
+++ src/main/java/com/star/demo001/App.java
@@ -0,0 +1,13 @@
+package com.star.demo001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
